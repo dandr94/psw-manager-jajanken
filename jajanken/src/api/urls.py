3243b81decdd65877e5620fc914142e562339b28
dandr94 <d.andreev1994@gmail.com>
2023-06-29 22:23:11 +0300

    Added history url

--- jajanken/src/api/urls.py
+++ jajanken/src/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main
+from .views import ListCreateNewPasswordEntryView, UpdatePasswordEntryView, ListHistoryView
 
 urlpatterns = [
-    path('', main, name='index')
+    path('home/', ListCreateNewPasswordEntryView.as_view(), name='index'),
+    path('update/<int:pk>/', UpdatePasswordEntryView.as_view(), name='update'),
+    path('history/', ListHistoryView.as_view(), name='history')
 ]
