ff5a679e013376c246a6d46f8b22a519f7f9d598
dandr94 <d.andreev1994@gmail.com>
2023-06-29 22:25:06 +0300

    Added new serializers...

--- jajanken/src/api/serializers.py
+++ jajanken/src/api/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from .models import PasswordManager
+from .models import History
 
 
 class PasswordManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model = PasswordManager
-        fields = ('website_name', 'website_url', 'website_account_name', 'website_password')
+        fields = ('id', 'website_name', 'website_url', 'website_account_name', 'website_password')
+
+
+class CreateNewPasswordEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PasswordManager
+        fields = ('id', 'website_name', 'website_url', 'website_account_name', 'website_password')
+
+
+class UpdatePasswordEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PasswordManager
+        fields = ('id', 'website_name', 'website_url', 'website_account_name', 'website_password')
+
+
+class ListHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = History
+        fields = ('id', 'status', 'website_name', 'status_changed')
