4f135a0d7d76bbe741c46fa6b7c5c49e9866aa9e
dandr94 <d.andreev1994@gmail.com>
2023-06-29 22:24:00 +0300

    Added patch for UpdatePasswordEntryView and added ListHistoryView

--- jajanken/src/api/views.py
+++ jajanken/src/api/views.py
@@ -1,8 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import History
+from .serializers import CreateNewPasswordEntrySerializer, UpdatePasswordEntrySerializer, ListHistorySerializer
+from .models import PasswordManager
 
-# Create your views here.
+
+class ListCreateNewPasswordEntryView(generics.ListCreateAPIView):
+    serializer_class = CreateNewPasswordEntrySerializer
+    queryset = PasswordManager.objects.all()
 
 
-def main(request):
-    return HttpResponse('Hello')
+class UpdatePasswordEntryView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UpdatePasswordEntrySerializer
+    queryset = PasswordManager.objects.all()
+
+    def patch(self, request, *args, **kwargs):
+        field = request.data.get('field')
+        value = request.data.get('value')
+        obj_id = kwargs['pk']
+        website_name = request.data.get('websiteName')
+
+        entry = PasswordManager.objects.get(id=obj_id)
+
+        setattr(entry, field, value)
+
+        entry.save()
+
+        history = History.objects.create(
+            status='Updated',
+            website_name=website_name,
+        )
+
+        return Response({'message': 'Account name updated successfully'})
+
+
+class ListHistoryView(generics.ListAPIView):
+    serializer_class = ListHistorySerializer
+    queryset = History.objects.all()
+
