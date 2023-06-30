3facdbcda818332cbf91dd837758ee6289f9edc0
dandr94 <d.andreev1994@gmail.com>
2023-06-30 23:54:04 +0300

    Added delete url

--- jajanken/src/api/urls.py
+++ jajanken/src/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListCreateNewPasswordEntryView, UpdatePasswordEntryView, ListHistoryView
+from .views import ListCreateNewPasswordEntryView, UpdateDeletePasswordEntryView, ListHistoryView
 
 urlpatterns = [
     path('home/', ListCreateNewPasswordEntryView.as_view(), name='index'),
-    path('update/<int:pk>/', UpdatePasswordEntryView.as_view(), name='update'),
+    path('update/<int:pk>/', UpdateDeletePasswordEntryView.as_view(), name='update'),
+    path('delete/<int:pk>/', UpdateDeletePasswordEntryView.as_view(), name='delete'),
     path('history/', ListHistoryView.as_view(), name='history')
 ]
