5c58a583d3941081224da8babac4dc30563c09d6
dandr94 <d.andreev1994@gmail.com>
2023-07-02 21:28:28 +0300

    Added JajankenUser to models.py

--- jajanken/src/api/models.py
+++ jajanken/src/api/models.py
@@ -1,5 +1,60 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.core.validators import MinLengthValidator
 from django.db import models
+from .managers import JajankenUserManager
+
+
+class JajankenUser(AbstractBaseUser, PermissionsMixin):
+    USERNAME_MAX_LEN = 15
+    USERNAME_MIN_LEN = 2
+    USERNAME_UNIQUE_ERROR_MESSAGE = 'Username is not available'
+
+    EMAIL_UNIQUE_ERROR_MESSAGE = 'This email is already used'
+
+    USERNAME_ERROR_MESSAGE = 'Username can contain only letters and numbers.'
+
+    username = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=USERNAME_MAX_LEN,
+        validators=[MinLengthValidator(USERNAME_MIN_LEN)],
+        error_messages={
+            'unique': USERNAME_UNIQUE_ERROR_MESSAGE
+        }
+    )
+
+    email = models.EmailField(
+        unique=True,
+        null=False,
+        blank=False,
+        error_messages={
+            'unique': EMAIL_UNIQUE_ERROR_MESSAGE
+        }
+
+    )
+    date_joined = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    is_staff = models.BooleanField(
+        default=False
+    )
+
+    is_superuser = models.BooleanField(
+        default=False
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = JajankenUserManager()
 
 
 class PasswordManager(models.Model):
@@ -40,7 +95,6 @@ class PasswordManager(models.Model):
 
 
 class History(models.Model):
-
     status_choices = [
         ('created', 'Created'),
         ('updated', 'Updated'),
