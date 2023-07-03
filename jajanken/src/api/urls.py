33e50adbf9bf6a4abdeda9829dbb2a65cdfd0d22
dandr94 <d.andreev1994@gmail.com>
2023-07-03 22:42:49 +0300

    Added auth urls

--- jajanken/src/api/urls.py
+++ jajanken/src/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import ListCreateNewPasswordEntryView, UpdateDeletePasswordEntryView, ListHistoryView
+from .views import ListPasswordEntriesView, UpdateDeletePasswordEntryView, ListHistoryView, CreateNewPasswordEntryView, \
+    RegisterUserView, LogoutUserView, LoginUserView
 
 urlpatterns = [
-    path('home/', ListCreateNewPasswordEntryView.as_view(), name='index'),
+    path('', ListPasswordEntriesView.as_view(), name='index'),
+    path('login/', LoginUserView.as_view(), name='register'),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('logout/', LogoutUserView.as_view(), name='logout'),
+    path('submit/', CreateNewPasswordEntryView.as_view(), name='create'),
     path('update/<int:pk>/', UpdateDeletePasswordEntryView.as_view(), name='update'),
     path('delete/<int:pk>/', UpdateDeletePasswordEntryView.as_view(), name='delete'),
     path('history/', ListHistoryView.as_view(), name='history')
