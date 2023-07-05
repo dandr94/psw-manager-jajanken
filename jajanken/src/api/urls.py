9de7f631fe2df56643dd9f1fc41a99b17acc3a54
dandr94 <d.andreev1994@gmail.com>
2023-07-05 22:32:48 +0300

    Added token urls and change '/' to dashboard

--- jajanken/src/api/urls.py
+++ jajanken/src/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import ListPasswordEntriesView, UpdateDeletePasswordEntryView, ListHistoryView, CreateNewPasswordEntryView, \
     RegisterUserView, LogoutUserView, LoginUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('', ListPasswordEntriesView.as_view(), name='index'),
+    path('dashboard/', ListPasswordEntriesView.as_view(), name='index'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', LoginUserView.as_view(), name='register'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
