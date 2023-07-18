bbb3e960fadc021885629e295ca3060e98524a30
dandr94 <d.andreev1994@gmail.com>
2023-07-18 22:32:54 +0300

    Added SubmenuAccountName.js

---
+++ jajanken/src/frontend/src/components/submenuFields/SubmenuAccountName.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import ViewButton from "../buttons/ViewButton";
+import EditButton from "../buttons/EditButton";
+import CopyButton from "../buttons/CopyButton";
+import { updateSubmenuFieldData } from "../../utils/requests";
+
+const SubmenuAccountName = ({ accountName, entryId }) => {
+    const [currentAccountName, setCurrentAccountName] = useState(accountName);
+    const [isAccountNameVisible, setIsAccountNameVisible] = useState(false);
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [editedAccountName, setEditedAccountName] = useState(accountName);
+    const websiteAccountField = "website_account_name";
+
+    const handleVisibilityToggle = () => {
+        setIsAccountNameVisible((prevState) => !prevState);
+    };
+
+    const handleEditModeToggle = () => {
+        setIsEditMode((prevEditMode) => !prevEditMode);
+    };
+
+    const handleaccountNameChange = (e) => {
+        setEditedAccountName(e.target.value);
+    };
+
+    const handleAccountNameSave = () => {
+        updateSubmenuFieldData(
+            entryId,
+            editedAccountName,
+            websiteAccountField,
+            setCurrentAccountName,
+            setIsEditMode
+        );
+    };
+
+    return (
+        <li>
+            <div className="group">
+                <span className="label">User/Email:</span>
+                {isEditMode ? (
+                    <input
+                        className="password-text"
+                        type="text"
+                        value={editedAccountName}
+                        onChange={handleaccountNameChange}
+                    />
+                ) : (
+                    <input
+                        className="password-text"
+                        type={isAccountNameVisible ? "text" : "password"}
+                        value={
+                            isAccountNameVisible
+                                ? currentAccountName
+                                : "********"
+                        }
+                        readOnly
+                    />
+                )}
+                <div className="card-buttons">
+                    <ViewButton
+                        isFieldVisible={isAccountNameVisible}
+                        onVisibilityToggle={handleVisibilityToggle}
+                    />
+                    <CopyButton textToCopy={accountName} />
+                    <EditButton
+                        isEditMode={isEditMode}
+                        onEditModeToggle={handleEditModeToggle}
+                        onSave={handleAccountNameSave}
+                    />
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default SubmenuAccountName;
