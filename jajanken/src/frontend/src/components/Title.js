79d2714c3acaf90008e6d8130670f555ee9ab253
dandr94 <d.andreev1994@gmail.com>
2023-07-19 17:30:57 +0300

    Added Title.css/js

---
+++ jajanken/src/frontend/src/components/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Title.css";
+
+function Title() {
+    return (
+        <h1 className="title">
+            <a className="title-href" href="/">
+                Jajanken
+            </a>
+        </h1>
+    );
+}
+
+export default Title;
