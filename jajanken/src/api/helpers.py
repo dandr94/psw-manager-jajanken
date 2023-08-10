8232131ce0607b9f8344201bf92426edea856027
dandr94 <d.andreev1994@gmail.com>
2023-08-10 23:32:33 +0300

    Added user to create_history_entry

--- jajanken/src/api/helpers.py
+++ jajanken/src/api/helpers.py
@@ -1,8 +1,9 @@
 from .models import History
 
 
-def create_history_entry(status: str, website_name: str) -> None:
+def create_history_entry(status: str, website_name: str, user) -> None:
     history = History.objects.create(
         status=status,
         website_name=website_name,
+        user=user
     )
